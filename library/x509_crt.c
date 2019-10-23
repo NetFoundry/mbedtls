a31dd8c0ed4e2fb451b7a21ceea454dd858abf8a
Eugene K <eugene.kobyakov@netfoundry.io>
2019-10-23 10:25:22 -0400

    do not use case-insensitive compare of IP addresses
    Signed-off-by: Eugene K <eugene.kobyakov@netfoundry.io>

--- library/x509_crt.c
+++ library/x509_crt.c
@@ -2980,10 +2980,10 @@ static int x509_crt_check_cn( const mbedtls_x509_buf *name,
 
         if (name->len == 4 &&
             inet_pton(AF_INET, cn, &ipv4) == 1 &&
-            x509_memcasecmp(&ipv4, name->p, name->len) == 0) {
+            memcmp(&ipv4, name->p, name->len) == 0) {
             return (0);
         } else if (inet_pton(AF_INET6, cn, &ipv6) == 1 &&
-            x509_memcasecmp(&ipv6, name->p, name->len) == 0) {
+            memcmp(&ipv6, name->p, name->len) == 0) {
             return (0);
         }
     }
